d97eb19ac8fd7d7a335ea5400411df59a98427a1
Richer Yang <fantasyworldidvtw@gmail.com>
2026-07-05 11:20:26 +0800

    調整延遲時間的說明

--- woocommerce/admin/settings/settings-invoice.php
+++ woocommerce/admin/settings/settings-invoice.php
@@ -63,13 +63,15 @@ return [
         'autoload' => false,
     ],
     [
-        'title' => __('Delay time', 'ry-invoice-for-smilepay'),
+        'title' => __('Delay time (hours)', 'ry-invoice-for-smilepay'),
         'id' => RY_IFSMILEPAY::OPTION_PREFIX . 'get_delay_time',
         'type' => 'number',
         'default' => '0',
-        'min' => '0',
-        'max' => '336',
-        'step' => '1',
+        'custom_attributes' => [
+            'min' => '0',
+            'max' => '336',
+            'step' => '1',
+        ],
         'desc' => __('After N hours get invoice.', 'ry-invoice-for-smilepay')
             . __('According to WordPress cron job, the actual execution time will be later than the specified time.', 'ry-invoice-for-smilepay'),
         'autoload' => false,
